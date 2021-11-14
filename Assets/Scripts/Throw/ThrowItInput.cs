5b2d3495297e4c5174f1c90b8d8d0e98205fc57a
Lyudmila5733 <millo4ka@list.ru>
2021-11-14 15:53:26 +0300

    Added the line rendering effect for swiping.

--- Assets/Scripts/Throw/ThrowItInput.cs
+++ Assets/Scripts/Throw/ThrowItInput.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,74 +10,71 @@ public class ThrowItInput : MonoBehaviour
     private Vector2 _startPos;
     private Vector2 _direction;
     private bool _isDirectionChosen;
+    private LineRenderer _line;
     
     public Vector2 SwipeDirection { get; private set; }
     public Vector2 SwipeStartPoint { get; private set; }
     public Vector2 SwipeEndPoint { get; private set; }
     public event UnityAction<Vector2> SwipeDone;
-    
+
+    private void Start()
+    {
+        _line = FindObjectOfType<LineRenderer>();
+    }
+
     private void Update()
     {
-// #if UNITY_ANDROID
-        // Track a single touch as a direction control.
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            // Handle finger movements based on touch phase.
-            switch (touch.phase)
-            {
-                // Record initial touch position.
-                case TouchPhase.Began:
-                    Debug.Log("INPUT Began : ");
-                    _startPos = touch.position;
-                    _isDirectionChosen = false;
-                    break;
-
-                // Determine direction by comparing the current touch position with the initial one.
-                case TouchPhase.Moved:
-                    // _direction = touch.position - _startPos;
-                    // Debug.Log("INPUT _direction : " + _direction);
-                    break;
-
-                // Report that a direction has been chosen when the finger is lifted.
-                case TouchPhase.Ended:
-                    _isDirectionChosen = true;
-                    
-                    Vector2 mousePoint = Input.mousePosition;
-                    _direction = mousePoint - _startPos;
-                    
-                    Debug.Log("INPUT Ended : ");
-                    Debug.Log("INPUT Ended _startPos : " + _startPos);
-                    Debug.Log("INPUT Ended : mousePoint : " + mousePoint);
-                    Debug.Log("INPUT Ended : distance : " + Vector2.Distance(_startPos, mousePoint));
-
-                    if (Vector2.Distance(_startPos, mousePoint)>5f)
-                        SwipeDone?.Invoke(_direction.normalized * 10);
-                    break;
-            }
-        }
-        if (_isDirectionChosen)
-        {
-            // Something that uses the chosen direction...
-            
-        }
-// #elif UNITY_EDITOR
+        CheckClicking();
+    }
+    
+    private void CheckClicking()
+    {
         if (Input.GetMouseButtonDown(0))
         {
+            RemoveLine();
             Vector2 mousePoint = Input.mousePosition;
-
             _startPos = mousePoint;
+            Debug.Log("ZZZAAA _startPos : " + _startPos);
+        } 
+        else if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Input.mousePosition;
+            // Debug.Log("ZZZ Distance(_startPos, mousePos) :" + Vector2.Distance(_startPos, mousePos));
+            // Debug.Log("ZZZ _startPos :" + _startPos);
+            // Debug.Log("ZZZ mousePos :" + mousePos);
+            if (Vector2.Distance(_startPos, mousePos) > 25f)
+            {
+                Camera cam = Camera.main;
+                
+                mousePos.x = Input.mousePosition.x;
+                mousePos.y = Input.mousePosition.y;
+                
+                var screenPos = new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane + 1);
+                var worldPos = cam.ScreenToWorldPoint(screenPos);
+                
+                _line.positionCount ++;
+                _line.SetPosition(_line.positionCount-1, worldPos);                
+            }
 
-        }
-
-        if (Input.GetMouseButtonDown(1))
+        } 
+        else if (Input.GetMouseButtonUp(0))
         {
             Vector2 mousePoint = Input.mousePosition;
             _direction = mousePoint - _startPos;
             
-            SwipeDone?.Invoke(_direction.normalized * 10);
-        }
-// #endif
+            Debug.Log("ZZZ Distance(_startPos, mousePos) :" + Vector2.Distance(_startPos, mousePoint));
+            // Debug.Log("AAA _endPos : " + mousePoint);
+            // Debug.Log("AAA _direction : " + _direction);
+            //
+            if (Vector2.Distance(_startPos, mousePoint)>25f)
+                SwipeDone?.Invoke(_direction.normalized * 10);
+            
+            Invoke(nameof(RemoveLine), 0.5f);
+        }        
+    }
+    
+    private void RemoveLine()
+    {
+        _line.positionCount = 0;
     }
 }
